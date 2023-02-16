452b4b67fb67486e79f918fddd038a9759f329d8
NatSama2 <vet.nat.c@gmail.com>
2023-02-15 22:57:04 -0300

    se guardo input

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+/*
+function muestraAlerta() {
+    if (checked="none") {
+        alert ("Por favor seleccione su compra")
+    }
+    else {
+      
+    }
+
+}*/
+
+
+
+/*
+document.getElementById("Confirmar").addEventListener("click", function() {
+
+}
+ */
+
+//Numero de tarjeta toma el valos del input
+let tarjetaVacia = " " ;
+
+document.getElementById("NumeroTarjeta").addEventListener("input", function (e) {
+  tarjetaVacia = e.target.value;
+});
+
+document.getElementById("Confirmar").addEventListener("click", function() {
+  document.getElementById("saludo").innerHTML =
+    "¡Hola " + tarjetaVacia + ", para comenzar selecciona el nivel.";
+});
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,30 @@
 const validator = {
-  // ...
+  isValid: function (cardNumber) {
+    //Numero de tarjeta toma el valos del input
+    let tarjetaVacia = " ";
+
+    document
+      .getElementById("NumeroTarjeta")
+      .addEventListener("input", function (e) {
+        tarjetaVacia = e.target.value;
+      });
+
+    //Trasformar número en array
+    const myArr = String(tarjetaVacia)
+      .split("")
+      .map((tarjetaVacia) => {
+        return Number(tarjetaVacia);
+      });
+
+    
+  }, 
 };
+
+document.getElementById("Confirmar").addEventListener("click", function () {
+  document.getElementById("saludo").innerHTML =
+    "¡Hola " + tarjetaVacia + ", para comenzar selecciona el nivel.";
+});
+
+
 
 export default validator;
