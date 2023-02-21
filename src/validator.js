0972f99b49de382b7e327d3326fa7a87c2a82ad9
NatSama2 <vet.nat.c@gmail.com>
2023-02-21 19:09:24 -0300

    validando

--- src/validator.js
+++ src/validator.js
@@ -9,48 +9,46 @@ const validator = {
     const cadena = strReverse;
     let array = cadena.split('');
     console.log('reversa', array);
+
+    //variables
     let multiplicacion;
     let sum;
     let sumaDeNumeros = 0;
-    let impares;
     
     //multiplicar x2 numeros en posicion pares
-    let pares = array.map ((num,pos) => {
-      if ((pos + 1) % 2 ===0 ) {
-      multiplicacion = num * 2 
+    for(let i = 0; i< array.length; i++) {
+      if ((i + 1) % 2 ===0 ) {
+      multiplicacion = array[i] * 2 
         console.log ('multiplicacion', multiplicacion)
         //ver si numero multiplicado es mayor a 9
       if (multiplicacion > 9) {
         let numTexto = multiplicacion.toString ();
         //Sumar los digitos individuales
-        sum = parseInt(numTexto[0]) + parseInt(numTexto[1]);
-        
-        console.log ('1er numero', numTexto [0])
-        console.log ('2er numero', numTexto [1])
-        console.log ('suma', sum)
-        return sum
+        sum = parseInt(numTexto[0]) + parseInt(numTexto[1])
+          array[i] = sum
+          console.log ('1er numero', numTexto [0])
+          console.log ('2er numero', numTexto [1])
+          console.log ('suma', sum)
         }
-        else {
-          return multiplicacion;
-        }
-
-
+       
       }
-      else {
-        impares = array
-      }
-      //sumar todos los numeros
-      sumaDeNumeros = array + sum + multiplicacion
-      return sumaDeNumeros; 
-      
-    })
+      console.log ('nuevo array',array)      
+    }
+     //sumar todos los numeros
+    for(let i = 0; i< array.length; i++) {
+      sumaDeNumeros += parseInt(array[i])
+
+     //validar que sea multiplo de 10
+    if (sumaDeNumeros % 10 == 0) {
+      console.log('TARJETA VALIDA (luhn)');
+      return true;
+  } else {
+      console.log('TARJETA INVALIDA (luhn)');
+      return false;
+  }
+    }
     console.log ('suma Total', sumaDeNumeros); 
-
-
-
-  
-
-        
+    return sumaDeNumeros; 
 
 
   }
