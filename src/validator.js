4511fce94b8d37dab46195e4f24f684d204d5c7b
NatSama2 <vet.nat.c@gmail.com>
2023-02-22 18:16:40 -0300

    al fi se valida

--- src/validator.js
+++ src/validator.js
@@ -19,36 +19,39 @@ const validator = {
     for(let i = 0; i< array.length; i++) {
       if ((i + 1) % 2 ===0 ) {
       multiplicacion = array[i] * 2 
-        console.log ('multiplicacion', multiplicacion)
+        /* console.log ('multiplicacion', multiplicacion) */
+        
         //ver si numero multiplicado es mayor a 9
       if (multiplicacion > 9) {
         let numTexto = multiplicacion.toString ();
         //Sumar los digitos individuales
         sum = parseInt(numTexto[0]) + parseInt(numTexto[1])
-          array[i] = sum
-          console.log ('1er numero', numTexto [0])
-          console.log ('2er numero', numTexto [1])
-          console.log ('suma', sum)
+          array[i] = parseInt(sum)
+          /*console.log ('1er numero', numTexto [0]);
+          console.log ('2er numero', numTexto [1]);
+          console.log ('suma', sum); */
         }
-       
+        else {
+          multiplicacion;
+        }       
       }
-      console.log ('nuevo array',array)      
+      console.log ('nuevo array',parseInt(array[i]))      
     }
      //sumar todos los numeros
     for(let i = 0; i< array.length; i++) {
       sumaDeNumeros += parseInt(array[i])
 
-     //validar que sea multiplo de 10
-    if (sumaDeNumeros % 10 == 0) {
-      console.log('TARJETA VALIDA (luhn)');
-      return true;
-  } else {
-      console.log('TARJETA INVALIDA (luhn)');
-      return false;
-  }
-    }
-    console.log ('suma Total', sumaDeNumeros); 
-    return sumaDeNumeros; 
+      console.log ('suma Total', sumaDeNumeros);       
+
+    }    
+         //validar que sea multiplo de 10
+         if (sumaDeNumeros % 10 == 0) {
+          console.log('TARJETA VALIDA (luhn)');
+          return true;
+      } else {
+          console.log('TARJETA INVALIDA (luhn)');
+          return false;
+      }
 
 
   }
