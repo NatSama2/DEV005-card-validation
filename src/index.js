39e397efa95e2a6d5b987177c9c1ec896c9d06c2
NatSama2 <vet.nat.c@gmail.com>
2023-02-28 14:50:29 -0300

    pasan los test, funciona maskify

--- src/index.js
+++ src/index.js
@@ -1,41 +1,51 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
 console.log(validator);
 
-let valid= validator.isValid;
+const valid = validator.isValid;
+const maskify = validator.maskify;
 
 //Guardar lo que diga en el input
 let tarjetaVacia = " ";
+let auxiliar = "";
 
-document.getElementById('NumeroTarjeta')
-  .addEventListener('input', function (e) {
+document
+  .getElementById("NumeroTarjeta")
+  .addEventListener("input", function (e) {
     tarjetaVacia = e.target.value;
-    let isValid = valid(tarjetaVacia);
+    const isValid = valid(tarjetaVacia);
     console.log("El número de tarjeta es válido: " + isValid);
   });
 
-  //ocultar ultimos 4 digitos
+//ocultar ultimos 4 digitos
 const input = document.getElementById("NumeroTarjeta");
-const maskify = (value) => {
-  const visible = value.slice(-4); // Obtiene los últimos 4 caracteres
-  const masked = value.slice(0, -4).replace(/./g, "#"); // Reemplaza todos los caracteres excepto los últimos 4 con "#"
-  return masked + visible; // Combina la cadena enmascarada y la visible
-};
+
 input.addEventListener("input", (event) => {
-  event.target.value = maskify(event.target.value);
+  auxiliar = event.target.value;
+
+  console.log("auxiliar", auxiliar);
 });
 
-  //boton confirmar
-  document.getElementById('Confirmar').addEventListener('click', function(e) {
-    e.preventDefault();
-    if (
-      valid == true) {
-        document.getElementById('finalizado').style.display = 'block';
-        document.getElementById('formulario').style.display = 'none';
+//boton confirmar
+document.getElementById("Confirmar").addEventListener("click", function (e) {
+  e.preventDefault();
+  input.value = maskify(auxiliar);
+  if (valid(tarjetaVacia) === true) {
+    document.getElementById("finalizado").style.display = "block";
+    document.getElementById("formulario").style.display = "none";
+  } else {
+    alert("Número de Tarjeta Invalido");
+  }
+});
 
-      }
-
-    else{
-      alert('Número de Tarjeta Invalido');
+//input Nombre
+let name = "";
+document
+  .getElementById("IngresarNombre")
+  .addEventListener("input", function (e) {
+    name = e.target.value;
+    if (name === "") {
+      document.getElementById("Nombre").innerHTML =
+        "Por favor complete este campo";
     }
-  })
+  });
